1e2518eba34fbf07c4d832ad8c870be0b6d84aa3
GetRighT <zhanglianbo35@126.com>
2016-11-04 22:47:13 +0800

    Update FTPs download the latest data.sh

--- FTPs download the latest data.sh
+++ FTPs download the latest data.sh
@@ -1,5 +1,11 @@
 #!/usr/local/bin/bash
-source /home/users/YOUR_username/.bashrc   #insert your username here 
+
+# I use this script to download the datasets package stored in JNJ FTP server,  including RAVE data & custom domain 
+# suppose you had setup a regular extra data through SASonDemand
+
+
+
+source /home/users/YOUR_username/.bashrc   #insert your username here , we must manually utilize the user's bash configure file at first
 ftpuser="yourftpusername"
 password="yourpassword"                    #if you have special character , you can quote your password
 protocol="JNJ protocol ID"
