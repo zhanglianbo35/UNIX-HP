a3fadd550937fe89e201bdc0f0d94f9bb19bb5e9
GetRighT <zhanglianbo35@126.com>
2016-11-04 22:25:03 +0800

    Update SDTM_transfer_tracker.sh

--- SDTM_transfer_tracker.sh
+++ SDTM_transfer_tracker.sh
@@ -1,4 +1,7 @@
 #colllect transfer info 
+# the script extra the info inside the define package of each transfer
+# package file name looks like  56022473AML2002_SDTM_prod_Transfer_20160503.zip
+
 zipinfo  -m "56022473AML2002_SDTM_*_Transfer_*.zip" > listfile.txt
 grep "number of entries" listfile.txt | awk -F: '{ print $(NF) - 6  }' > test.txt
 grep "Archive:" listfile.txt | awk -F_ 'BEGIN{OFS="\t"} { print "56022473AML2002" , $2 , toupper($3) , substr($5,1,8) }' > test2.txt
