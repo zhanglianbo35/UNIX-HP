3b4c20d09e5a7b5c601fdcb46ebdeee9f3c0a3f7
GetRighT <zhanglianbo35@126.com>
2016-11-14 15:28:11 +0800

    Update upload2sdd.sh
    also upload a SDTM copy to PK office folder when on the begin of each month

--- upload2sdd.sh
+++ upload2sdd.sh
@@ -3,10 +3,19 @@ source  /opt/pxlcommon/stats/applications/sdd/setcp
 
 local_definedir=/projects/jjprd224002/stats/transfer/outputs/define
 sdd_definedir=/SAS/3952/56022473AML2002/Files/Staging/DM_CRO/SDTM_XPT_Package/Current
+input4pk=/SAS/3952/56022473AML2002/Files/Staging/DM_CRO/Input_Data_For_PK_Office
 
 current_sdtm=$(cd ${local_definedir} ;\
 ls -l *prod*.zip | awk -F_ '{print substr($(NF),1,8) , $0}' | sort -k1n | awk 'END{print $(NF)}' );\
 echo -e $current_sdtm  "   will be upload to SDD"
 
- /opt/java6/bin/java sample.SASDrugDevCommand -s https://sddcampine.ondemand.sas.com -u LZHAN134 -p sdd_password  \
+ /opt/java6/bin/java sample.SASDrugDevCommand -s https://sddcampine.ondemand.sas.com -u ${sdduser} -p ${sddpassword}  \
 -create_repository_file ${sdd_definedir}/${current_sdtm} ${local_definedir}/${current_sdtm}
+
+export thisday=$(date +%d) 
+if (( $thisday + 0 <= 7 ))
+then   
+  echo "send an copy to SG PK office" 
+  /opt/java6/bin/java sample.SASDrugDevCommand -s https://sddcampine.ondemand.sas.com -u ${sdduser} -p ${sddpassword}  \
+  -create_repository_file ${input4pk}/${current_sdtm} ${local_definedir}/${current_sdtm}
+fi
