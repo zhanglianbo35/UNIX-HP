9cff44d75adb4db00d9d001d6e7ad36b95eeb224
GetRighT <zhanglianbo35@126.com>
2016-12-19 10:36:18 +0800

    Update FTPs download the latest data.sh

--- FTPs download the latest data.sh
+++ FTPs download the latest data.sh
@@ -17,7 +17,7 @@ dir_cust=/projects/${project}/stats/transfer/data/rawcust
 
 type=${type^^}
 protocol=${protocol^^}
-/opt/pdag/bin/curl  --ftp-pasv  --ftp-ssl -u ${ftpuser}:${password} -k ftp://jnj2.ftp.mdsol.com/${protocol}/${type}/ > ftpdownload.txt
+/usr/local/bin/curl  --ftp-pasv  --ftp-ssl -u ${ftpuser}:${password} -k ftp://jnj2.ftp.mdsol.com/${protocol}/${type}/ > ftpdownload.txt
 awk -F_  -v protocol=$protocol  '{ if ($0 ~ ("_" protocol) ) print "ravedata", $(NF-1), substr($(NF),1,6) , $0 ; if ($0 ~ (" " protocol))  print "cust", $(NF-1), substr($(NF),1,6) , $0 }' ftpdownload.txt > ftpdownload1.txt
 
 filelst=`sort -t" " -k2rn -k3rn < ftpdownload1.txt | awk '!x[$1]++' | sort -k1 | awk -F' ' '{if ($1=="cust") print "latestcust="$(NF); if ($1=="ravedata") print "latestraw="$(NF)}'`
